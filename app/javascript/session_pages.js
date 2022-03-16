4a3ff39be68a38171a304db45098c8fcbdfee307
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-03-16 12:55:23 -0500

    Category page UI

--- app/javascript/session_pages.js
+++ app/javascript/session_pages.js
@@ -3,24 +3,13 @@ const submitLogIn = () => {
 }
 
 const submitSignUp = () => {
-
     let photo = document.getElementById("upload_photo")
-    
-
     if(!photo.value.length){
-
-
         photo.value = "https://www.w3schools.com/howto/img_avatar.png"
-
-
     }
-
-
     document.getElementById("new_user").submit()
 }
 
 const uploadButtonInfo = () => {
     alert("You can paste the URL of your image or the system will generate a image for you")
 }
-
-
