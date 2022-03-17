95d9ca9a8012e7873c61ebe98f1e52da0f824b94
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-03-17 01:03:18 -0500

    Transaction UI and show total feature

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,7 +1,12 @@
 class TransactionsController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
+  def index
+  
+    @category = Category.find(params[:category_id])
+    
+
+  end
 
   def create; end
 end
