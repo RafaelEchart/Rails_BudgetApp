f18ede9118afbba26184d9f7ce9991d32c37044a
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-03-18 01:54:50 -0500

    Initial Main Commit

--- app/controllers/splash_screen_controller.rb
+++
@@ -1,5 +0,0 @@
-class SplashScreenController < ApplicationController
-  def index
-    redirect_to categories_index_path if user_signed_in?
-  end
-end
--- app/models/entity_category.rb
+++
@@ -1,4 +0,0 @@
-class EntityCategory < ApplicationRecord
-  belongs_to :entity
-  belongs_to :category
-end
