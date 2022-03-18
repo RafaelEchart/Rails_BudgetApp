291ddcb34d3fae1df4ff2c9e1f5310e5c083d0e4
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-03-17 20:54:25 -0500

    View testing completed

--- spec/views/categories_page_spec.rb
+++ spec/views/categories_page_spec.rb
@@ -6,7 +6,7 @@ describe 'User category page', type: :feature do
     login_user
   end
 
-  it 'I can see the name of the category of User' do
+  it 'I can see Total category banner of user' do
     visit '/categories'
     expect(page).to have_content('Total categories')
   end
@@ -15,10 +15,22 @@ describe 'User category page', type: :feature do
     visit '/categories'
     expect(page).to have_css('div')
   end
+  
+  it 'The category page should have button to add new category' do
+    visit '/categories'
+    expect(page).to have_content('New Category')
+  end
 
   it 'I can see the button to open menu' do
     visit "/categories/#{@category.id}/transactions"
     expect(page).to have_content('Total')
+  end
+
+  it 'I can see the username and password inputs and the "Log in" button.' do
+    visit "/categories/new"
+    expect(page).to have_css("input[id$='category_name']")
+    expect(page).to have_css("select[id$='category_icon']")
+    expect(page).to have_content('Add')
   end
 
 end
---
+++ spec/views/transactions_page_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+require 'capybara/rspec'
+
+describe 'User category page', type: :feature do
+  before :each do
+    login_user
+  end
+
+  it 'I can see the total category banner for user' do
+    visit "/categories/#{@category.id}/transactions"
+    expect(page).to have_content('Total')
+  end
+  
+  it 'The category page should have button to create new transaction' do
+    visit "/categories/#{@category.id}/transactions"
+    expect(page).to have_content('New transaction')
+  end
+
+  it 'I can see the transaction header title' do
+    visit "/categories/#{@category.id}/transactions"
+    expect(page).to have_content('TRANSACTIONS')
+  end
+  
+  it 'I can see the username and password inputs and the "Log in" button.' do
+    visit "/categories/#{@category.id}/transactions/new"
+    expect(page).to have_css("input[id$='entity_name']")
+    expect(page).to have_css("input[id$='entity_amount']")
+    expect(page).to have_content('Add')
+  end
+
+end
